4c16b05ead2a315e3e09c18f5c0ae6aec108d105
Tolga Dur <tdur1995@gmail.com>
2019-07-04 15:42:09 +0100

    updated get_csv and visualisation with new data from pvtu

--- get_csv.py
+++ get_csv.py
@@ -17,62 +17,74 @@ import vtktools
 #        OUTPUT: average_over_time.csv                                         #
 ################################################################################
 
-ug = vtktools.vtu('data/raw_data/LSBU_200.vtu')
+# ug = vtktools.vtu('data/raw_data/LSBU_500.vtu')
+# ug.GetFieldNames()
+# pos = ug.GetLocations()
+# tracer = ug.GetScalarField('Tracer')
+# tracer_background = ug.GetScalarField('TracerBackground')
+# pressure = ug.GetScalarField('Pressure')
+# velocity = ug.GetVectorNorm('Velocity')
+#
+# ##################### FUNCTION TO COMBINE DATA IN DATAFRAME ####################
+#
+# def getDF(pos, velocity, pressure, tracer_background, tracer):
+#     df = pd.DataFrame({'X': pos[:, 0],
+#                        'Y': pos[:, 1],
+#                        'Z': pos[:, 2],
+#                        'velocity_norm': velocity,
+#                        'pressure': pressure,
+#                        'tracer_background': tracer_background,
+#                        'tracer': tracer})
+#     return df
+#
+# ######################## GET AVERAGE OVER TIMESTEPS ############################
+#
+# for i in range(201, 989):
+#     last_index = i+1
+#     print('LSBU_'+str(i)+'.vtu')
+#     ug = vtktools.vtu('data/raw_data/LSBU_'+str(i)+'.vtu')
+#     ug.GetFieldNames()
+#     pos = ug.GetLocations()
+#
+#     tracer += ug.GetScalarField('Tracer')
+#     tracer_background += ug.GetScalarField('TracerBackground')
+#     pressure += ug.GetScalarField('Pressure')
+#     velocity += ug.GetVectorNorm('Velocity')
+#
+# tracer /= last_index
+# tracer_background /= last_index
+# pressure /= last_index
+# # velocity /= last_index
+#
+# df = getDF(pos, velocity, pressure, tracer_background, tracer)
+#
+# ################ NORMALIZE/STANDARDIZE POTENTIAL OUTPUT FIELDS #################
+#
+# def normalize(df, column):
+#     return (df[column]-df[column].min()) / (df[column].max()-df[column].min())
+#
+# def standardize(df, column):
+#     print(df[column].mean())
+#     return (df[column]-df[column].mean()) / df[column].std()
+#
+# df['tracer'] = standardize(df, 'tracer')
+# df['tracer_background'] = standardize(df, 'tracer_background')
+# df['pressure'] = standardize(df, 'pressure')
+# df['velocity_norm'] = standardize(df, 'velocity_norm')
+
+############################## GET PVTU_RAW CSV ################################
+
+ug = vtktools.vtu('data/pvtu_raw/LSBU_500/LSBU_500_9.vtu')
 ug.GetFieldNames()
 pos = ug.GetLocations()
-tracer = ug.GetScalarField('Tracer')
-tracer_background = ug.GetScalarField('TracerBackground')
-pressure = ug.GetScalarField('Pressure')
-velocity = ug.GetVectorNorm('Velocity')
+tracer = ug.GetScalarField('TracerGeorge')
 
-##################### FUNCTION TO COMBINE DATA IN DATAFRAME ####################
-
-def getDF(pos, velocity, pressure, tracer_background, tracer):
-    df = pd.DataFrame({'X': pos[:, 0],
-                       'Y': pos[:, 1],
-                       'Z': pos[:, 2],
-                       'velocity_norm': velocity,
-                       'pressure': pressure,
-                       'tracer_background': tracer_background,
-                       'tracer': tracer})
-    return df
-
-######################## GET AVERAGE OVER TIMESTEPS ############################
-
-for i in range(201, 989):
-    last_index = i+1
-    print('LSBU_'+str(i)+'.vtu')
-    ug = vtktools.vtu('data/raw_data/LSBU_'+str(i)+'.vtu')
-    ug.GetFieldNames()
-    pos = ug.GetLocations()
-
-    tracer += ug.GetScalarField('Tracer')
-    tracer_background += ug.GetScalarField('TracerBackground')
-    pressure += ug.GetScalarField('Pressure')
-    velocity += ug.GetVectorNorm('Velocity')
-
-tracer /= last_index
-tracer_background /= last_index
-pressure /= last_index
-# velocity /= last_index
-
-df = getDF(pos, velocity, pressure, tracer_background, tracer)
-
-################ NORMALIZE/STANDARDIZE POTENTIAL OUTPUT FIELDS #################
-
-def normalize(df, column):
-    return (df[column]-df[column].min()) / (df[column].max()-df[column].min())
-
-def standardize(df, column):
-    print(df[column].mean())
-    return (df[column]-df[column].mean()) / df[column].std()
-
-df['tracer'] = standardize(df, 'tracer')
-df['tracer_background'] = standardize(df, 'tracer_background')
-df['pressure'] = standardize(df, 'pressure')
-df['velocity_norm'] = standardize(df, 'velocity_norm')
+df = pd.DataFrame({'X': pos[:, 0],
+                   'Y': pos[:, 1],
+                   'Z': pos[:, 2],
+                   'tracer': tracer})
 
 ######################### COMBINE AND SAVE DATA ################################
 
 print('Saving...')
-df.to_csv('timestep_500.csv', index=False)
+df.to_csv('PVTU_500_9.csv', index=False)
--- visualization.py
+++ visualization.py
@@ -10,9 +10,20 @@ import GPy
 #        in this project.                                                      #
 ################################################################################
 
-average_not_stand = pd.read_csv('data/csv_data/not_standardized/average_time.csv')
-time_500_not_stand = pd.read_csv('data/csv_data/not_standardized/timestep_500.csv')
-time_500_stand = pd.read_csv('data/csv_data/standardized/timestep_500.csv')
+# average_not_stand = pd.read_csv('data/csv_data/not_standardized/average_time.csv')
+# time_500_not_stand = pd.read_csv('data/csv_data/not_standardized/timestep_500.csv')
+# time_500_stand = pd.read_csv('data/csv_data/standardized/timestep_500.csv')
+pvtu_500_0 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_0.csv')
+pvtu_500_1 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_1.csv')
+pvtu_500_2 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_2.csv')
+pvtu_500_3 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_3.csv')
+pvtu_500_4 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_4.csv')
+pvtu_500_5 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_5.csv')
+pvtu_500_6 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_6.csv')
+pvtu_500_7 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_7.csv')
+pvtu_500_8 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_8.csv')
+pvtu_500_9 = pd.read_csv('data/csv_data/pvtu_500_not_standardized/PVTU_500_9.csv')
+
 
 ################################# HISTOGRAMS ###################################
 
@@ -29,22 +40,34 @@ def showHistogram(data, title, number_bins=400):
     printHistogramInformation(title, bins, counts)
     plt.show()
 
-# Tracer
-showHistogram(average_not_stand['tracer'], 'Avg. Time: Tracer-Histogram (Not Standardized)')
-showHistogram(time_500_not_stand['tracer'], 'Timestep 500: Tracer-Histogram (Not Standardized)')
-showHistogram(time_500_stand['tracer'], 'Timestep 500: Tracer-Histogram (Standardized)')
+# TracerGeorge
+showHistogram(PVTU_500_0['tracer'], 'PVTU_500_0: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_1['tracer'], 'PVTU_500_1: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_2['tracer'], 'PVTU_500_2: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_3['tracer'], 'PVTU_500_3: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_4['tracer'], 'PVTU_500_4: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_5['tracer'], 'PVTU_500_5: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_6['tracer'], 'PVTU_500_6: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_7['tracer'], 'PVTU_500_7: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_8['tracer'], 'PVTU_500_8: TracerGeorge-Histogram (Not Standardized)')
+showHistogram(PVTU_500_9['tracer'], 'PVTU_500_9: TracerGeorge-Histogram (Not Standardized)')
 
-# Tracer Background
-showHistogram(average_not_stand['tracer_background'], 'Avg. Time: TracerBackground-Histogram (Not Standardized)')
-showHistogram(time_500_not_stand['tracer_background'], 'Timestep 500: TracerBackground-Histogram (Not Standardized)')
-showHistogram(time_500_stand['tracer_background'], 'Timestep 500: TracerBackground-Histogram (Standardized)')
-
-# Pressure
-showHistogram(average_not_stand['pressure'], 'Avg. Time: Pressure-Histogram (Not Standardized)')
-showHistogram(time_500_not_stand['pressure'], 'Timestep 500: Pressure-Histogram (Not Standardized)')
-showHistogram(time_500_stand['pressure'], 'Timestep 500: Pressure-Histogram (Standardized)')
-
-# Velocity Norm
-showHistogram(average_not_stand['velocity_norm'], 'Avg. Time: VelocityNorm-Histogram (Not Standardized)')
-showHistogram(time_500_not_stand['velocity_norm'], 'Timestep 500: VelocityNorm-Histogram (Not Standardized)')
-showHistogram(time_500_stand['velocity_norm'], 'Timestep 500: VelocityNorm-Histogram (Standardized)')
+# # Tracer
+# showHistogram(average_not_stand['tracer'], 'Avg. Time: Tracer-Histogram (Not Standardized)')
+# showHistogram(time_500_not_stand['tracer'], 'Timestep 500: Tracer-Histogram (Not Standardized)')
+# showHistogram(time_500_stand['tracer'], 'Timestep 500: Tracer-Histogram (Standardized)')
+#
+# # Tracer Background
+# showHistogram(average_not_stand['tracer_background'], 'Avg. Time: TracerBackground-Histogram (Not Standardized)')
+# showHistogram(time_500_not_stand['tracer_background'], 'Timestep 500: TracerBackground-Histogram (Not Standardized)')
+# showHistogram(time_500_stand['tracer_background'], 'Timestep 500: TracerBackground-Histogram (Standardized)')
+#
+# # Pressure
+# showHistogram(average_not_stand['pressure'], 'Avg. Time: Pressure-Histogram (Not Standardized)')
+# showHistogram(time_500_not_stand['pressure'], 'Timestep 500: Pressure-Histogram (Not Standardized)')
+# showHistogram(time_500_stand['pressure'], 'Timestep 500: Pressure-Histogram (Standardized)')
+#
+# # Velocity Norm
+# showHistogram(average_not_stand['velocity_norm'], 'Avg. Time: VelocityNorm-Histogram (Not Standardized)')
+# showHistogram(time_500_not_stand['velocity_norm'], 'Timestep 500: VelocityNorm-Histogram (Not Standardized)')
+# showHistogram(time_500_stand['velocity_norm'], 'Timestep 500: VelocityNorm-Histogram (Standardized)')
