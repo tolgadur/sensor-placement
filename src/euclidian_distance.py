bb7e49e0266bdb7d7e3e3fe73608f1fca68318cb
Tolga Dur <tdur1995@gmail.com>
2019-08-19 20:10:50 +0100

    wrote script to calculate euclidian distance

---
+++ src/euclidian_distance.py
@@ -0,0 +1,16 @@
+import math
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('./solutions/LSBU32/area_6_8/output_absolute_4sensoren.csv', dtype=np.float64)
+data = data.values
+
+average_8 = 0
+for i in range(3):
+    average_8 += math.sqrt(sum([(a - b) ** 2 for a, b in zip(data[i], data[i+1])]))
+print(average_8/4)
+
+average_6 = 0
+for i in range(4, 7):
+    average_6 += math.sqrt(sum([(a - b) ** 2 for a, b in zip(data[i], data[i+1])]))
+print(average_6/4)
