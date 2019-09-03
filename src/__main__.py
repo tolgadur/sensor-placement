185d4630dfed04106b21bc4da30afe39176f45ec
Tolga Dur <tdur1995@gmail.com>
2019-09-03 14:39:55 +0100

    changed main file

--- src/__main__.py
+++ src/__main__.py
@@ -12,22 +12,21 @@ from time import time
 
 def main():
     """ Description """
-    MagicProject.plotHistogram(subdomain=8, number_bins=5)
-    MagicProject.describeData(subdomain=8)
-    MagicProject.plotResiuals(subdomain=8, number_bins=800)
-    exit(0)
+    # MagicProject.plotHistogram(subdomain=6, number_bins=5)
+    # MagicProject.describeData(subdomain=6)
+    # MagicProject.plotResiuals(subdomain=6, number_bins=800)
 
     """ Validation """
-    print(MagicProject.validation_random(subdomain=6, k=1))
-
-    indLoc = np.loadtxt('./solutions/subdomain_8/validation_format/num_sens/7sens.txt', dtype=int)
-    print(MagicProject.validation(subdomain=8, A=indLoc))
+    # print(MagicProject.validation_random(subdomain=6, k=1))
+    #
+    # indLoc = np.loadtxt('./solutions/subdomain_8/validation_format/num_sens/7sens.txt', dtype=int)
+    # print(MagicProject.validation(subdomain=8, A=indLoc))
 
     """ Calling functions to optimize sensor placement """
-    t0 = time()
-    A = MagicProject.parallelPlacement(subdomains=[1, 2], k=4, algorithm=4)
-    t1 = time()
-    print('The parallel placement algorithm takes ', (t1-t0), 'seconds')
+    # t0 = time()
+    # A = MagicProject.parallelPlacement(subdomains=[1, 2], k=4, algorithm=4)
+    # t1 = time()
+    # print('The parallel placement algorithm takes ', (t1-t0), 'seconds')
 
     t0 = time()
     A = MagicProject.simplePlacement(subdomain=6, k=4, algorithm=3)
@@ -35,7 +34,7 @@ def main():
     print('The non-parallel placement algorithm takes ', (t1-t0), 'seconds')
 
     """ Printing the optimizing sensor placements """
-    V1_df = pd.read_csv('data/csv_data/subdomain_19/positions.csv')
+    V1_df = pd.read_csv('data/csv_data/subdomain_6/positions.csv')
     V1 = V1_df[['X', 'Y', 'Z']].copy().values
     # V2_df = pd.read_csv('data/csv_data/subdomain_8/positions.csv')
     # V2 = V2_df[['X', 'Y', 'Z']].copy().values
